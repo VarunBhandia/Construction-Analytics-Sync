944102c39c9fb26fbadf114e8d28c42ad4bf8d60
Varun Bhandia <varunbhandia@gmail.com>
2018-06-21 15:17:56 +0530

    debugging

---
+++ application/controllers/material_rqst.php
@@ -0,0 +1,128 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+
+	class Material_rqst extends CI_Controller
+	{
+		public $table = 'test';
+		public $controller = 'material_rqst';
+		public $message = 'Construction';
+		public $primary_id = "mrid";
+		public $model;
+		
+		public function __construct()
+		{
+			parent::__construct();
+			$this->load->model('material_rqst_m');
+			$this->model = 'material_rqst_m';
+			date_default_timezone_set('Asia/Kolkata');
+		}
+	
+		public function index()
+		{
+			$model = $this->material_rqst_m;
+			$data['controller'] = $this->controller;
+			$data['row'] = $this->$model->select(array(),'sitedetails',array(),'');
+			//$data['row'] = $this->$model->db_query("select * from test INNER JOIN vendor ON `vendor`.id = `test`.vendor");
+			$this->load->view('po/index',$data);
+		}
+		
+		public function form()
+		{
+			$model = $this->model;
+			$data['action'] = "insert";
+			$data['controller'] = $this->controller;
+			$data['material_unit'] = $this->$model->select(array(),'munit',array(),'');
+			$data['materials'] = $this->$model->select(array(),'material',array(),'');
+			$this->load->view('po/form',$data);
+		}
+
+		public function insert()
+		{
+			$model = $this->model;
+			
+			$site = $this->input->post('site');
+			$date = date('Y-m-d',strtotime($this->input->post('date')));
+
+			$mname = count($this->input->post('mname')) > 0 ? implode(",",$this->input->post('mname')) : $this->input->post('mname');
+			
+			$qty = count($this->input->post('qty')) > 0 ? implode(",",$this->input->post('qty')) : $this->input->post('qty');
+			
+			$unit = count($this->input->post('unit')) > 0 ? implode(",",$this->input->post('unit')) : $this->input->post('unit');
+			
+			$m_unit = count($this->input->post('m_unit')) > 0 ? implode(",",$this->input->post('m_unit')) : $this->input->post('m_unit');
+			
+			$remark = count($this->input->post('remark')) > 0 ? implode(",",$this->input->post('remark')) : $this->input->post('remark');
+			
+			$data = array(
+					'site'  => $site,
+					'receive_date'  => $date,
+					'mname' => $mname,
+					'qty'  => $qty,
+					'unit_price'  => $unit,
+					'material_unit'  => $m_unit,
+					'remarks'  => $remark
+				);
+			
+			$this->$model->insert($data,$this->table);
+			
+			$this->session->set_flashdata('add_message','<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button>Added Successfully!</div>');
+			
+			redirect('material_rqst');
+		}
+
+		public function edit($mrid)
+		{
+			$model = $this->model;
+			$data['row'] = $this->$model->select(array(),$this->table,array($this->mrid=>$mrid),'');
+			$data['materials'] = $this->$model->select(array(),'material',array(),'');
+			$data['action'] = "update";
+			$data['controller'] = $this->controller;
+			$this->load->view('material_rqst/form',$data);
+		}
+
+		public function update()
+		{
+			$model = $this->model;
+
+			$site = $this->input->post('site');
+			$date = date('Y-m-d',strtotime($this->input->post('date')));
+
+			$material = count($this->input->post('material')) > 0 ? implode(",",$this->input->post('material')) : $this->input->post('material');
+			
+			$qty = count($this->input->post('qty')) > 0 ? implode(",",$this->input->post('qty')) : $this->input->post('qty');
+			
+			$unit = count($this->input->post('unit')) > 0 ? implode(",",$this->input->post('unit')) : $this->input->post('unit');
+			
+			$m_unit = count($this->input->post('m_unit')) > 0 ? implode(",",$this->input->post('m_unit')) : $this->input->post('m_unit');
+			
+			$remark = count($this->input->post('remark')) > 0 ? implode(",",$this->input->post('remark')) : $this->input->post('remark');
+			
+			$data = array(
+					'site'  => $site,
+					'receive_date'  => $date,
+					'material' => $material,
+					'qty'  => $qty,
+					'unit_price'  => $unit,
+					'material_unit'  => $m_unit,
+					'remarks'  => $remark
+				);
+			$this->session->set_flashdata('add_message','<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button>Updated Successfully!</div>');
+			
+			$id = $this->input->post('mrid');
+			$where = array($this->mrid=>$mrid);
+			$this->$model->update($this->table,$data,$where);
+			
+			redirect('material_rqst');
+		}
+
+		public function delete($mrid)
+		{
+			$model = $this->model;
+			$condition = array($this->mrid=>$mrid);
+			$this->$model->delete($this->table,$condition);
+			
+			$this->session->set_flashdata('add_message','<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert"><i class="ace-icon fa fa-times"></i></button>Deleted Successfully!</div>');
+			redirect('My_controller');
+		}
+	}
+?>

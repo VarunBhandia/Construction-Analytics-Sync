c0eb2cc88e420a1bcdcab96bfd910a1eff60960e
Varun Bhandia <varunbhandia@gmail.com>
2018-07-09 00:32:48 +0530

    ui updates last commit

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,68 +1,68 @@
- <!-- footer content -->
-        <footer>
-          <div class="">
-Made with by Varun Bhandia | Tushar Goyal          </div>
-          <div class="clearfix"></div>
-        </footer>
-        <!-- /footer content -->
-      </div>
-    </div>
+<!-- footer content -->
+<footer>
+    <div class="">
+        Made with <i class="fa fa-heart" style="color:red"></i> by <a href="https://www.linkedin.com/in/varunbhandia/" target="_blank">Varun Bhandia</a> and <a href="https://www.linkedin.com/in/tushar-goyal-3a1616ba/" target="_blank">Tushar Goyal</a> .        </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
 <!--
-                       <script type="text/javascript">
-      $('.form-control').select2({
-        placeholder: '--- Select Sites ---',
-        });
+<script type="text/javascript">
+$('.form-control').select2({
+placeholder: '--- Select Sites ---',
+});
 </script>
 -->
 
-    <!-- jQuery -->
-    <script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
-    <!-- Bootstrap -->
-    <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
-    <!-- bootstrap-progressbar -->
-    <script src="<?php echo base_url();?>assets/js/bootstrap-progressbar.min.js"></script>
-    <!-- Skycons -->
-    <script src="<?php echo base_url();?>assets/js/skycons.js"></script>
-    <!-- DateJS -->
-    <script src="<?php echo base_url();?>assets/js/date.js"></script>
-	
-	<!--Data Table-->
-	<script src="<?php echo base_url();?>assets/js/jquery.dataTables.min.js"></script>
-	<script src="<?php echo base_url();?>assets/js/dataTables.bootstrap.min.js"></script>
-		
-    <!-- bootstrap-daterangepicker -->
-    <script src="<?php echo base_url();?>assets/js/moment.min.js"></script>
-	
-    <!-- Custom Theme Scripts -->
-    <script src="<?php echo base_url();?>assets/js/custom.min.js"></script>
-	
-	<!-- Include Date Range Picker -->
-	<script src="<?php echo base_url();?>assets/js/bootstrap-datepicker.min.js"></script>
-	
-	<!-- Copy Data -->
-	<script src="<?php echo base_url();?>assets/js/dataTables.buttons.min.js"></script>
-	
-	<!-- Comman Js -->
-	<script src="<?php echo base_url();?>assets/js/common.js"></script>
-    
-	<script>
+<!-- jQuery -->
+<script src="<?php echo base_url();?>assets/js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="<?php echo base_url();?>assets/js/bootstrap-progressbar.min.js"></script>
+<!-- Skycons -->
+<script src="<?php echo base_url();?>assets/js/skycons.js"></script>
+<!-- DateJS -->
+<script src="<?php echo base_url();?>assets/js/date.js"></script>
+
+<!--Data Table-->
+<script src="<?php echo base_url();?>assets/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url();?>assets/js/dataTables.bootstrap.min.js"></script>
+
+<!-- bootstrap-daterangepicker -->
+<script src="<?php echo base_url();?>assets/js/moment.min.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="<?php echo base_url();?>assets/js/custom.min.js"></script>
+
+<!-- Include Date Range Picker -->
+<script src="<?php echo base_url();?>assets/js/bootstrap-datepicker.min.js"></script>
+
+<!-- Copy Data -->
+<script src="<?php echo base_url();?>assets/js/dataTables.buttons.min.js"></script>
+
+<!-- Comman Js -->
+<script src="<?php echo base_url();?>assets/js/common.js"></script>
+
+<script>
     $(document).ready(function() {
-    $('#example').DataTable();
-	} );
+        $('#example').DataTable();
+    } );
 </script>
 <script>
-	function delet()
-	{
-		if(confirm("Do You Really Delete Record"))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
+    function delet()
+    {
+        if(confirm("Do You Really Delete Record"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 </script>
-	
-  </body>
+
+</body>
 </html>
--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,84 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="<?php echo base_url();?>assets/images/favicon.ico" />
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!-- Meta, title, CSS, favicons, etc. -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="<?php echo base_url();?>assets/images/favicon.ico" />
 
-    <title>Codeigniter | </title>
+        <title>Codeigniter | </title>
 
-    <!-- Bootstrap -->
-    <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo base_url();?>assets/css/mycss.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="<?php echo base_url();?>assets/css/font-awesome.min.css" rel="stylesheet">
-    <!-- bootstrap-progressbar -->
-    <link href="<?php echo base_url();?>assets/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
-    <!-- bootstrap-daterangepicker -->
-    <link href="<?php echo base_url();?>assets/css/daterangepicker.css" rel="stylesheet">
-	<!-- bootstrap-datetimepicker -->
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-datepicker3.css"/>
-	
-    <link href="<?php echo base_url();?>assets/css/bootstrap-datetimepicker.css" rel="stylesheet">
-	
-	<!-- Datatable -->
-	<link href="<?php echo base_url();?>assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
-	
-	<!--Copy Data-->
-	<link href="<?php echo base_url();?>assets/css/buttons.bootstrap.min.css" rel="stylesheet">
+        <!-- Bootstrap -->
+        <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo base_url();?>assets/css/mycss.css" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="<?php echo base_url();?>assets/css/font-awesome.min.css" rel="stylesheet">
+        <!-- bootstrap-progressbar -->
+        <link href="<?php echo base_url();?>assets/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+        <!-- bootstrap-daterangepicker -->
+        <link href="<?php echo base_url();?>assets/css/daterangepicker.css" rel="stylesheet">
+        <!-- bootstrap-datetimepicker -->
+        <link rel="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap-datepicker3.css"/>
 
-    <!-- Custom Theme Style -->
-    <link href="<?php echo base_url();?>assets/css/custom.min.css" rel="stylesheet">
+        <link href="<?php echo base_url();?>assets/css/bootstrap-datetimepicker.css" rel="stylesheet">
 
-<!--     Select2 JS and CSS Files -->
-    <script src="<?php echo base_url('assets/js/jquery-3.2.1.min.js')?>" type='text/javascript'></script>
-    <script src="<?php echo base_url('assets/select2/dist/js/select2.min.js')?>" type='text/javascript'></script>
+        <!-- Datatable -->
+        <link href="<?php echo base_url();?>assets/css/dataTables.bootstrap.min.css" rel="stylesheet">
 
-    <link href="<?php echo base_url('assets/select2/dist/css/select2.min.css')?>" rel='stylesheet' type='text/css'>
-	    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-  </head>
+        <!--Copy Data-->
+        <link href="<?php echo base_url();?>assets/css/buttons.bootstrap.min.css" rel="stylesheet">
 
-  <body class="nav-md">
-    <div class="container body">
-      <div class="main_container">
-        <div class="col-md-3 left_col">
-          <div class="left_col scroll-view">
-            <div class="navbar nav_title" style="border: 0;">
-              <a href="<?php echo base_url();?>" class="site_title"><i class="fa fa-paw"></i> <span>Construction</span></a>
-            </div>
+        <!-- Custom Theme Style -->
+        <link href="<?php echo base_url();?>assets/css/custom.min.css" rel="stylesheet">
 
-            <div class="clearfix"></div>
+        <!--     Select2 JS and CSS Files -->
+        <script src="<?php echo base_url('assets/js/jquery-3.2.1.min.js')?>" type='text/javascript'></script>
+        <script src="<?php echo base_url('assets/select2/dist/js/select2.min.js')?>" type='text/javascript'></script>
 
-            <br />
+        <link href="<?php echo base_url('assets/select2/dist/css/select2.min.css')?>" rel='stylesheet' type='text/css'>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+    </head>
 
-            <!-- sidebar menu -->
-            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
-              <div class="menu_section">
-                <ul class="nav side-menu">
-                  
-					<li><a href="<?php echo base_url().$this->controller;?>">Construction</a></li>
-				  </ul>
-              </div>
+    <body class="nav-md">
+        <div class="container body">
+            <div class="main_container">
+                <div class="col-md-3 left_col">
+                    <div class="left_col scroll-view">
+                <div class="col-md-3 left_col">
+                    <div class="left_col scroll-view">
+                        <div class="navbar nav_title" style="border: 0;">
+                            <a href="<?php echo base_url();?>" class="site_title">Construction Analytics 2018</a>
+                        </div>
 
-            </div>
-            <!-- /sidebar menu -->
-          </div>
-        </div>
+                        <!-- sidebar menu -->
+                        <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                            <div class="menu_section">
+                            </div>
 
-        <!-- top navigation -->
-        <div class="top_nav">
-          <div class="nav_menu">
-            <nav>
-              <div class="nav toggle">
-                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-              </div>
+                        </div>
+                        <!-- /sidebar menu -->
+                    </div>
+                </div>
 
-            </nav>
-          </div>
-        </div>
-        <!-- /top navigation -->
+                <!-- top navigation -->
+                <div class="top_nav">
+                    <div class="nav_menu">
+                        <nav>
+                            <div class="nav toggle">
+                                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                            </div>
+
+                        </nav>
+                    </div>
+                </div>
+                <!-- /top navigation -->

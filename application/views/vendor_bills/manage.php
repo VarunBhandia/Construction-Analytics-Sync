19078e95900178bcb56d3d014074d224e9e6dd5f
Varun Bhandia <varunbhandia@gmail.com>
2018-07-12 15:03:50 +0530

    excel

--- application/views/vendor_bills/manage.php
+++ application/views/vendor_bills/manage.php
@@ -101,7 +101,8 @@ error_reporting(0);
                             <div class="col-md-12 col-sm-12 col-xs-12">
                                 <div class="x_panel">
                                     <div class="x_title">
-                                        <h2>Vendor Bill Details</h2>
+                                        <h1>Vendor Bill Details</h1>
+<!--
                                         <ul class="nav navbar-right panel_toolbox">
                                             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                             </li>
@@ -109,6 +110,7 @@ error_reporting(0);
                                             <li><a class="close-link"><i class="fa fa-close"></i></a>
                                             </li>
                                         </ul>
+-->
                                         <div class="clearfix"></div>
                                     </div>
                                     <div class="x_content">

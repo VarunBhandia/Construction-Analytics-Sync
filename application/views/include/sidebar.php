8cbd61ed0d9ddae5d32ebb057b02e148a0e6ed3f
Varun Bhandia <varunbhandia@gmail.com>
2018-07-15 16:13:44 +0530

    sidebar

---
+++ application/views/include/sidebar.php
@@ -0,0 +1,126 @@
+<?php
+$user_module = $user_roles[0]->user_role;
+$site_module = $user_roles[0]->site_role;
+$material_module = $user_roles[0]->material;
+$vendor_module = $user_roles[0]->vendor;
+$mr_module = $user_roles[0]->mr;
+$po_module = $user_roles[0]->po;
+$rtv_module = $user_roles[0]->rtv;
+$cp_module = $user_roles[0]->cp;
+$uogrn_module = $user_roles[0]->uogrn;
+$vendorbills_module = $user_roles[0]->vendorbills;
+$vendorbillpayment_module = $user_roles[0]->vendorbillpayment;
+$moveorder_module = $user_roles[0]->moveorder;
+$officegstdetails_module = $user_roles[0]->officegstdetails;
+$subcontractor_module = $user_roles[0]->subcontractor;
+$transporter_module = $user_roles[0]->transporter;
+$workorder_module = $user_roles[0]->workorder;
+$reporting_module = $user_roles[0]->reporting;
+$workordermaterials_module = $user_roles[0]->workordermaterials;
+$consumption_module = $user_roles[0]->consumption;
+?>
+
+
+<div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+    <div class="menu_section">
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+        <a href="<?php echo base_url(); ?>user" target="_blank">
+            <div class="SidebarModuleTile">
+                <div class="SidebarModuleHeading">
+                    USER
+                </div>
+            </div>
+        </a>
+    </div>
+
+</div>

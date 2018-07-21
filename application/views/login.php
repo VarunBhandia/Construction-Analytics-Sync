a9e8543d9e871756e357ede93108aa6c67a9a40b
Varun Bhandia <varunbhandia@gmail.com>
2018-07-21 19:20:37 +0530

    ui bug

--- application/views/login.php
+++ application/views/login.php
@@ -7,15 +7,15 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="<?php echo base_url(); ?>images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor1/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor1/animate/animate.css">
 <!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor1/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>vendor1/select2/select2.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/util.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/main.css">
@@ -59,14 +59,14 @@
      </div>
 
             <!--===============================================================================================-->	
-	<script src="<?php echo base_url(); ?>vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="<?php echo base_url(); ?>vendor1/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url(); ?>vendor/bootstrap/js/popper.js"></script>
-	<script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>vendor1/bootstrap/js/popper.js"></script>
+	<script src="<?php echo base_url(); ?>vendor1/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url(); ?>vendor/select2/select2.min.js"></script>
+	<script src="<?php echo base_url(); ?>vendor1/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-	<script src="<?php echo base_url(); ?>vendor/tilt/tilt.jquery.min.js"></script>
+	<script src="<?php echo base_url(); ?>vendor1/tilt/tilt.jquery.min.js"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
